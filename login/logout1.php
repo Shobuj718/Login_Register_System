086207816fd107a1bb6d054fa6130d05ffb16b4a
shobuj <shobujsa93@gmail.com>
2017-08-02 11:23:30 +0600

    logout1 file added comments

--- login/logout1.php
+++ login/logout1.php
@@ -5,7 +5,7 @@
 
 		$user = new LoginRegistration();
 		$user->logOutUser();
-		header("Location: login1.php?response=1");
+		header("Location: login1.php?response=1"); //redirects the login1 file
 		exit();
 
 ?>
